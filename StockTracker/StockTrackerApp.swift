19891b18f07ea0967540cb79fb287df1f5511928
Mohammed Abdelaty <mohamed.abdelaaty@btech.com>
2025-11-28 00:50:02 +0200

    Initial Commit

---
+++ StockTracker/StockTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTrackerApp.swift
+//  StockTracker
+//
+//  Created by Mohammed Abdelaty on 28/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct StockTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StockTrackerTests/StockTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  StockTrackerTests.swift
+//  StockTrackerTests
+//
+//  Created by Mohammed Abdelaty on 28/11/2025.
+//
+
+import Testing
+@testable import StockTracker
+
+struct StockTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
